1f7770ed2beaeeb8d0ac833868d3e3499c8baf32
amzrk2 <amzrk2@outlook.com>
2020-12-20 14:56:23 +0800

    new: redirect page

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <EContainer class="error">
+    <ECard>
+      <div class="wrapper"></div>
+    </ECard>
+  </EContainer>
+</template>
+
+<style>
+.error .wrapper {
+  padding: var(--space-lg);
+}
+</style>
