47087d9ed1ed52a41135d7dbd39eeca773590fce
amzrk2 <amzrk2@outlook.com>
2020-12-21 22:18:19 +0800

    new: scss loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 /*! dsr-index | DSRKafuU <amzrk2.cc> | Copyright (c) Apache 2.0 License */
+
+/* css */
+import 'normalize.css';
+import './scss/index.scss';
+
+/* js */
 import { logInfo } from './utils/logger.js';
 
 document.body.textContent = 'home page';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const env = process.env.NODE_ENV || 'production';
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -18,9 +19,24 @@ module.exports = {
   optimization: {
     minimizer: [new TerserWebpackPlugin({ extractComments: false })],
   },
+  module: {
+    rules: [
+      // css & scss
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     // clean last built files
     new CleanWebpackPlugin(),
+    // create css files
+    new MiniCssExtractPlugin(),
     // create html files
     new HtmlWebpackPlugin({
       template: './src/index.html',
