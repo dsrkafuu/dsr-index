1809bb005f8db3b03a4763aa08ba63b829020131
amzrk2 <amzrk2@outlook.com>
2020-12-22 15:40:37 +0800

    fix: cdn path

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const env = process.env.NODE_ENV || 'production';
 const path = require('path');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -7,57 +6,63 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: env,
-  // set entry and output dist
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash:6].js',
-    publicPath: '/',
-  },
-  optimization: {
-    minimizer: [new TerserWebpackPlugin({ extractComments: false })],
-  },
-  module: {
-    rules: [
-      // css & scss
-      {
-        test: /\.s?[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      // inline svg required in html template
-      {
-        test: /\.svg$/,
-        loader: 'raw-loader',
-        options: {
-          esModule: false,
+module.exports = (env) => {
+  const BASE_PATH = '/';
+  const CDN_BASE_PATH = 'https://cdn.jsdelivr.net/gh/amzrk2/dsr-cdn@1/';
+
+  return {
+    // set entry and output dist
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash:6].js',
+      publicPath: BASE_PATH,
+    },
+    optimization: {
+      minimizer: [new TerserWebpackPlugin({ extractComments: false })],
+    },
+    module: {
+      rules: [
+        // css & scss
+        {
+          test: /\.s?[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
         },
-      },
-      // files
-      {
-        test: /\.(jpe?g|png|gif|webp|ico|woff2?|[to]tf)$/i,
-        loader: 'file-loader',
-        options: {
-          esModule: false,
-          name: '[name].[contenthash:6].[ext]',
-          outputPath: 'assets',
+        // inline svg required in html template
+        {
+          test: /\.svg$/,
+          loader: 'raw-loader',
+          options: {
+            esModule: false,
+          },
         },
-      },
-    ],
-  },
-  plugins: [
-    // clean last built files
-    new CleanWebpackPlugin(),
-    // extract css files from loader
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash:6].css',
-    }),
-    // create html files use `html-loader` and `svg-inline-loader`
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      minify:
-        env === 'production'
+        // files
+        {
+          test: /\.(jpe?g|png|gif|webp|ico|woff2?|[to]tf)$/i,
+          loader: 'file-loader',
+          options: {
+            // prevent no default issue
+            esModule: false,
+            // output to dist/* with same path as src/*
+            context: 'src',
+            // remove hash when using cdn
+            name: `[path][name]${env.production ? '' : '.[contenthash:6]'}.[ext]`,
+            publicPath: env.production ? CDN_BASE_PATH : BASE_PATH,
+          },
+        },
+      ],
+    },
+    plugins: [
+      // clean last built files
+      new CleanWebpackPlugin(),
+      // extract css files from loader
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash:6].css',
+      }),
+      // create html files use `html-loader` and `svg-inline-loader`
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        minify: env.production
           ? {
               // custom
               collapseBooleanAttributes: true,
@@ -71,14 +76,16 @@ module.exports = {
               useShortDoctype: true,
             }
           : false,
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: 'static' }],
-    }),
-  ],
-  devServer: {
-    contentBase: 'dist',
-    compress: true,
-    port: 9000,
-  },
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'static' }],
+      }),
+    ],
+    devServer: {
+      contentBase: 'dist',
+      compress: true,
+      port: 9000,
+    },
+    devtool: env.production ? false : 'cheap-module-source-map',
+  };
 };
