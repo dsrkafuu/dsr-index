34af7a387660d386da1c56eea16c3fd90d3572df
amzrk2 <amzrk2@outlook.com>
2020-12-25 16:34:08 +0800

    fix: webkit animation issue

--- src/js/utils/image.js
+++ src/js/utils/image.js
@@ -4,16 +4,31 @@
  * @param {String} src
  */
 function loadImage(container, src, alt) {
+  // loading start time
+  const st = Date.now();
+
   return new Promise((resolve, reject) => {
     const image = new Image();
     alt && (image.alt = alt);
-    image.onload = () => {
-      container.appendChild(image);
-      resolve();
-    };
+
     image.onerror = (e) => {
       reject(e);
     };
+    image.onload = () => {
+      // at lease 500ms loading time
+      const lt = 500 - (Date.now() - st);
+
+      if (lt >= 4) {
+        setTimeout(() => {
+          container.appendChild(image);
+          resolve();
+        }, lt);
+      } else {
+        container.appendChild(image);
+        resolve();
+      }
+    };
+
     image.src = src;
   });
 }
