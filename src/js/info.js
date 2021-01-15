4f3e5073af676859b31982d211c42e684b85d132
amzrk2 <amzrk2@outlook.com>
2021-01-15 19:52:28 +0800

    update: remove avatar alt text

--- src/js/info.js
+++ src/js/info.js
@@ -15,7 +15,7 @@ async function initInfo() {
 
     // init avatar
     const wrapper = document.querySelector('.info:not(.skt) .avatar .image');
-    await loadImage(wrapper, webp ? webpAvatar : jpgAvatar, 'avatar');
+    await loadImage(wrapper, webp ? webpAvatar : jpgAvatar);
     logInfo('Avatar loaded');
 
     // trigger animation
