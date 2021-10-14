ca664b02973586da3211d145c71b08a46e7175ab
dsrkafuu <dsrkafuu@outlook.com>
2021-10-14 14:11:41 +0800

    build: fix minify script issue

--- minify.js
+++ minify.js
@@ -4,6 +4,7 @@ const html = require('html-minifier-terser');
 const postcss = require('postcss');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
+const chalk = require('chalk');
 
 // files needs to be processed
 const allFiles = {
@@ -46,36 +47,39 @@ function getFiles(dirPath) {
 }
 
 const t = Date.now();
-console.log('fetching raw files...');
+console.log(chalk.blue('fetching raw files...'));
 getFiles(path.resolve(__dirname, './public'));
-console.log(`raw files fetched in ${Date.now() - t}ms`);
+console.log(chalk.green(`raw files fetched in ${Date.now() - t}ms`));
 
 // minify
 const t1 = Date.now();
-console.log('processing html files...');
+console.log(chalk.blue('processing html files...'));
 const htmlWorkers = [];
 allFiles['.html'].forEach((val) => {
   htmlWorkers.push(
     new Promise((resolve, reject) => {
       try {
         const content = fs.readFileSync(val, { encoding: 'utf8' });
-        const result = html.minify(content, {
-          collapseBooleanAttributes: true,
-          collapseWhitespace: true,
-          ignoreCustomComments: [/^!/, /^\s*#/],
-          keepClosingSlash: true,
-          removeComments: true,
-          removeRedundantAttributes: true,
-          removeScriptTypeAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          sortAttributes: true,
-          sortClassName: true,
-          useShortDoctype: true,
-          minifyCSS: true,
-          minifyJS: true,
-        });
-        fs.writeFileSync(val, result);
-        resolve();
+        html
+          .minify(content, {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            ignoreCustomComments: [/^!/, /^\s*#/],
+            keepClosingSlash: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            sortAttributes: true,
+            sortClassName: true,
+            useShortDoctype: true,
+            minifyCSS: true,
+            minifyJS: true,
+          })
+          .then((result) => {
+            fs.writeFileSync(val, result);
+            resolve();
+          });
       } catch (e) {
         reject(e);
       }
@@ -83,11 +87,11 @@ allFiles['.html'].forEach((val) => {
   );
 });
 Promise.all(htmlWorkers)
-  .then(() => console.log(`done with html files in ${Date.now() - t1}ms`))
+  .then(() => console.log(chalk.green(`done with html files in ${Date.now() - t1}ms`)))
   .catch((e) => console.error(e));
 
 const t2 = Date.now();
-console.log('processing css files...');
+console.log(chalk.blue('processing css files...'));
 const cssWorkers = [];
 allFiles['.css'].forEach((val) => {
   cssWorkers.push(
@@ -110,5 +114,5 @@ allFiles['.css'].forEach((val) => {
   );
 });
 Promise.all(cssWorkers)
-  .then(() => console.log(`done with css files in ${Date.now() - t2}ms`))
+  .then(() => console.log(chalk.green(`done with css files in ${Date.now() - t2}ms`)))
   .catch((e) => console.error(e));
