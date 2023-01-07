a2d312b022a03e0b00fcf478909e16f4e76fec0f
dsrkafuu <dsrkafuu@outlook.com>
2023-01-07 14:35:48 +0800

    fix: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../utils/global.scss';
 import type { AppProps } from 'next/app';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 import { ChakraProvider } from '../utils/chakra';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider cookies={pageProps.cookies}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider cookies={pageProps.cookies}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <GoogleAnalytics trackPageViews />
+    </>
   );
 }
 
