444a3239deca767e85033a7094af7f589c85d64c
dsrkafuu <dsrkafuu@outlook.com>
2023-02-18 19:25:24 +0800

    fix: license

---
+++ README.md
@@ -0,0 +1,9 @@
+## DSRIndeX
+
+Index page of all DSR domains.
+
+### License
+
+Released under the `GNU AGPLv3`, for more information read the [License](https://github.com/dsrkafuu/dsr-index/blob/main/LICENSE).
+
+**Copyright © 2020-present DSRKafuU**
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { defineConfig } from 'vite';
 import { splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -5,11 +6,19 @@ import svgr from 'vite-plugin-svgr';
 import htmlEnv from 'vite-plugin-html-env';
 import { visualizer } from 'rollup-plugin-visualizer';
 
+const NODE_ENVS: any = process.env || {};
+
 /**
  * https://vitejs.dev/config/
  */
 export default defineConfig({
-  plugins: [splitVendorChunkPlugin(), htmlEnv(), react(), svgr(), visualizer()],
+  plugins: [
+    splitVendorChunkPlugin(),
+    htmlEnv({ ...NODE_ENVS }),
+    react(),
+    svgr(),
+    visualizer(),
+  ],
   build: {
     target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
   },
