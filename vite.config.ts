5fab72dd5b53a79ead85bb9dc6fef0771e6611dd
dsrkafuu <dsrkafuu@outlook.com>
2023-02-18 19:21:51 +0800

    fix: html env

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import { splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import htmlEnv from 'vite-plugin-html-env';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 /**
  * https://vitejs.dev/config/
  */
 export default defineConfig({
-  plugins: [splitVendorChunkPlugin(), react(), svgr(), visualizer()],
+  plugins: [splitVendorChunkPlugin(), htmlEnv(), react(), svgr(), visualizer()],
   build: {
     target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
   },
